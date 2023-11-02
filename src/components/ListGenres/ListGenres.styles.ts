fa4f8aff187f70ccbcaa662aa32650694eab1f7e
Felipe Okino <felipe1okino@live.com>
2023-11-02 03:33:43 -0300

    feat: created component to show genres

---
+++ src/components/ListGenres/ListGenres.styles.ts
@@ -0,0 +1,11 @@
+import { Button } from '@chakra-ui/react';
+import styled from 'styled-components';
+
+export const ButtonComponent = styled(Button)`
+    background-color: #f1f1f1;
+    cursor: pointer;
+    outline: none;
+    border: none;
+    border-radius: 5px;
+    width: 100px;
+`;
