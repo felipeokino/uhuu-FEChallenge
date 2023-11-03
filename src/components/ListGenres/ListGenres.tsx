2b2f915ab9c41108d8f1c0917b28abc7b6440d28
Felipe Okino <felipe1okino@live.com>
2023-11-03 14:36:16 -0300

    styles: updated layout of list genres

--- src/components/ListGenres/ListGenres.tsx
+++ src/components/ListGenres/ListGenres.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { useGenres } from '../../hooks/useGenres';
-import { MenuButton, MenuItem, MenuList, Menu } from '@chakra-ui/react';
-import { ButtonComponent } from './ListGenres.styles';
-import { useSearchParams } from 'react-router-dom';
 
+import React from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { useGenres } from '../../hooks/useGenres';
+import { ButtonComponent, Container } from './ListGenres.styles';
+import { IoCloseCircleSharp } from 'react-icons/io5';
 export default function ListGenres(){
   const { genres } = useGenres();
   const [ selectedGenre, setSelectedGenre ] = React.useState<number[]>([]);
   const [ filters , setFilters ] = useSearchParams();
 
-  const handleChange = (genre: number) => {
+  const handleClick = (genre: number) => {
     const selected = new Set(selectedGenre);
 
     if (selected.has(genre)) 
@@ -18,57 +18,36 @@ export default function ListGenres(){
       selected.add(genre);
     
     setSelectedGenre(Array.from(selected));
+    applyFilter();
   };
 
-  const handleClose = () => {  
+  const applyFilter = () => {  
     filters.set('with_genres', (selectedGenre||[]).join('|'));
     setFilters(filters);
   };
 
   return (
-    <div>
-      <Menu onClose={handleClose}>
-        <MenuButton as={ButtonComponent} >
-              Genres
-        </MenuButton>
-        <MenuList>
-          <>
-            <MenuItem 
-              bg={'#c5c5c5'}
-              color={'#303030'}
-              outline={'none'} 
-              border={'none'}
-              padding={'4px 3px'}
-              margin={'2px'}
-              onClick={() => {
-                setSelectedGenre([]);
-              }} 
-              closeOnSelect={true}
-              cursor={'pointer'}
-            >
-              Clear All
-            </MenuItem>
-            {genres.map(({ id, name }) => (
-              <MenuItem 
-                cursor={'pointer'}
-                outline={'none'} 
-                border={'none'}
-                padding={'4px 3px'}
-                margin={'2px'}
-                key={id} 
-                bg={selectedGenre.includes(id) ? '#303030': '#f1f1f1'}
-                color={!selectedGenre.includes(id) ? '#303030': '#f1f1f1'}
-                
-                onClick={() => handleChange(id)} 
-                closeOnSelect={false}
-              >
-                {name}
-              </MenuItem>
-            ))}
-          </>
-        </MenuList>
-      </Menu>
-    </div>
+    <Container>
+      <p>
+        Milhões de filmes, séries e pessoas para descobrir. Explore já.
+      </p>
+      <div>
+        <span>FILTRE POR:</span>
+        {genres.map(({ id, name }) => (
+          <ButtonComponent 
+            selected={selectedGenre.includes(id)} 
+            key={id} 
+            onClick={() => handleClick(id)}
+          >
+            {name}
+            {selectedGenre.includes(id) && (
+              <IoCloseCircleSharp size={16} fill={'var(--neutral-color)'}/>
+            )}
+          </ButtonComponent>
+        ))}
+      </div>
+       
+    </Container>
   );
 }
 
