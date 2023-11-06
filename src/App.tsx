93532037c5066bc91f9bb03dec17ceb8b5473393
Felipe Okino <felipe1okino@live.com>
2023-11-06 10:50:48 -0300

    feat: fixed routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,26 @@
-import React, { useMemo, useState } from 'react';
-
-import { Context } from './contexts/movies';
-import { TMovieBasic } from './@types/movies';
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Home from './pages/MoviesByPopularity';
-import Details from './pages/Details';
 import Layout from './components/Layout';
-import MoviesByGenres from './pages/MoviesByGenres';
-import Header from './components/Header';
+import Details from './pages/Details';
+import Home from './pages/MoviesByPopularity';
+import GlobalStyles from './styles/global';
 
 
 export default function App() {
   return (
-    <Layout>
+    <>
+      <GlobalStyles  />
       <BrowserRouter basename='/'>
-        <Header />
-        <Routes>
-          <Route Component={Home} path='/popular'  />
-          <Route Component={MoviesByGenres} path='/discover'  />
-          <Route Component={Details} path='/movies/:id'  />
-        </Routes>
+        <Layout>
+          <Routes>
+            <Route Component={Home} path='/'  />
+            <Route Component={Details} path='/movies/:id'  />
+          </Routes>
+        </Layout>
+        
       </BrowserRouter>
-    </Layout>
+    </>
   );
 }
 
